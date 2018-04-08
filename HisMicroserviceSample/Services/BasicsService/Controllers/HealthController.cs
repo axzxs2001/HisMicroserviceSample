89e591672509a0e06b461d7383e12a9c2d2150ae
桂素伟 <axzxs2001@163.com>
2018-04-08 13:36:06 +0800

    更新PPT。 添加健康检查路由。

---
+++ HisMicroserviceSample/Services/BasicsService/Controllers/HealthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicsService.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        /// <summary>
+        /// 主健康检查
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/health")]
+        public IActionResult KeyHealth()
+        {
+            return Content("BasicsService KeyHealth检查成功！");
+        }
+    }
+}
